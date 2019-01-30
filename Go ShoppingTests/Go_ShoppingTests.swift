25b9b04f32309fc0ea5a683ecde7365de0891853
Davi <davipneto@hotmail.com>
2019-01-30 17:14:03 -0200

    Initial Commit

---
+++ Go ShoppingTests/Go_ShoppingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Go_ShoppingTests.swift
+//  Go ShoppingTests
+//
+//  Created by Davi Pereira Neto on 30/01/19.
+//  Copyright © 2019 Davi Pereira Neto. All rights reserved.
+//
+
+import XCTest
+@testable import Go_Shopping
+
+class Go_ShoppingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
